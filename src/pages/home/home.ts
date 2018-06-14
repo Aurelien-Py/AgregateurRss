af73859bf267acec3b8e679bf7175bc646f3374a
Theo <theo.pierret1@gmail.com>
2018-06-14 11:01:46 +0200

    Ajout de la recuperation des flux et des feeds sur le home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FluxProvider } from '../../providers/flux/flux';
 import 'rxjs/add/operator/map';
+import { Feed } from '../../models/Feed/feed';
+import { Flux } from '../../models/Flux/flux';
 
 @Component({
   selector: 'page-home',
@@ -10,22 +12,40 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage implements AfterViewInit, OnInit {
 
-  
+  feeds: Array<Feed>;
 
   constructor(public navCtrl: NavController, public http: HttpClient, public flux: FluxProvider) {
-
+    this.feeds = Array<Feed>(0);
   }
 
   ngAfterViewInit(){
+    this.flux.add(new Flux('Journal du Hacker','https://www.journalduhacker.net/rss'));
+    this.flux.add(new Flux('Korben','https://korben.info/feed'));
+    this.flux.add(new Flux('test','https://www.journalduhacker.net/rss'));
+    this.doRefresh();
+  }
 
-   this.http.get('https://rss2json.com/api.json?rss_url=https://www.journalduhacker.net/rss')
-    .subscribe((result) => {
-        console.log(result);
-    }, (error) => {
-        console.log(error);
-    });
+  doRefresh(){
+    this.flux.getAll().then(
+      data => {
+        console.log(data);
+        data.forEach(element => {
+          this.http.get('https://rss2json.com/api.json?rss_url=' + element.link)
+          .subscribe((result) => {
+              
+            console.log(result);
+    
+    
+          }, (error) => {
+              console.log(error);
+          });
+          
+        });
+      },
+      error => {
 
-
+      }
+    )    
   }
 
   ngOnInit(){
