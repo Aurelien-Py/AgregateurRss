7fe20c9449fb679c2858803f9ef00970d49114e8
Theo <theo.pierret1@gmail.com>
2018-06-20 11:25:43 +0200

    Recuperation des categories Creation d'un flux via le modal de creation Affichage d'un toast

--- src/pages/modal-adding-flux/modal-adding-flux.ts
+++ src/pages/modal-adding-flux/modal-adding-flux.ts
@@ -1,7 +1,10 @@
+import { Category } from './../../models/Category/category';
 import { Component } from '@angular/core';
 import { Flux } from '../../models/Flux/flux';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { FluxProvider } from '../../providers/flux/flux';
+import { CategoryProvider } from '../../providers/category/category';
+import { ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the ModalAddingFluxPage page.
@@ -20,9 +23,18 @@ export class ModalAddingFluxPage {
 
   nameFlux: string;
   addressFlux: string;
-  //categoryFlux: Category;
+  categoryFlux: Category;
+  listCategories: Array<Category>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public flux: FluxProvider,public viewC: ViewController) {
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController, public navParams: NavParams, public flux: FluxProvider,public viewC: ViewController, public category: CategoryProvider) {
+    this.category.getAll().then(
+      data => {
+        this.listCategories = data;
+      },
+      error => {
+        console.log("Error while loading categories");
+      }
+    )
   }
 
   ionViewDidLoad() {
@@ -34,7 +46,14 @@ export class ModalAddingFluxPage {
   }
   
   addFlux(){
-    //this.flux.add(new Flux(this.nameFlux,this.addressFlux);
+    this.flux.add(new Flux(this.nameFlux,this.addressFlux, this.categoryFlux));
+    this.closeModal();
+    let toast = this.toastCtrl.create({
+      message: 'Flux ajouté',
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
   }
 
 }
