fb509d8ec3c9e3c46477ae62b078a90c7320ac06
Aurélien <aurelienpy0@gmail.com>
2018-06-21 09:07:46 +0200

    Ajout image random

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -17,9 +17,13 @@ import { Feed } from '../../models/Feed/feed';
 export class FeedPage {
 
   public feed: Feed;
-
+  public nombre: number;
+  public listImages = [ 'assets/imgs/Image1.jpg', 'assets/imgs/Image2.jpg','assets/imgs/Image3.jpg','assets/imgs/Image4.jpg'];
+  public image;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.feed = navParams.get('feed');
+    this.randomImage();
+    
   }
 
   ionViewDidLoad() {
@@ -29,6 +33,11 @@ export class FeedPage {
   openlink(feed: Feed){
     window.open(feed.link);
   }
-  
+  randomImage() {
+    let min = 0;
+    let max = 4;
+    this.nombre = Math.floor((Math.random() * (max - min))+ min);
+    this.image = this.listImages[this.nombre];
+  }
 
 }
