95bb7dd68baeb8a908342127e6e2c0555706ce24
sye9286 <sye.9286@gmail.com>
2018-06-26 11:48:19 +0200

    correction bouton +

--- src/pages/list-flux/list-flux.ts
+++ src/pages/list-flux/list-flux.ts
@@ -2,7 +2,7 @@
 
 
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, Modal } from 'ionic-angular';
 
 import { FluxProvider } from "../../providers/flux/flux";
 import { Flux } from "../../models/Flux/flux";
@@ -23,7 +23,7 @@ export class ListFluxPage {
 
   ListFlux: Array<Flux> ;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public FluxP: FluxProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public FluxP: FluxProvider, private modal: ModalController) {
     this.ListFlux = Array<Flux>(0) ;
     this.FluxP.getAll().then(data =>{
       this.ListFlux = data ;
@@ -35,7 +35,10 @@ export class ListFluxPage {
     console.log('ionViewDidLoad ListFluxPage');
   }
 
-  
+  openModal(){
+    const modalAddFlux : Modal = this.modal.create('ModalAddingFluxPage');
+    modalAddFlux.present();
+  }
 
 }
 
