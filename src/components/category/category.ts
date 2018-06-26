ee0d5a219d0730a4c59ee162f9ae41f8509211d0
esther <esther.m.meyer@wanadoo.fr>
2018-06-26 12:08:36 +0200

    Category adding, customing, deleting

--- src/components/category/category.ts
+++ src/components/category/category.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { Category } from '../../models/Category/category';
+import { CategoryProvider } from '../../providers/category/category';
 
 /**
  * Generated class for the CategoryComponent component.
@@ -16,8 +17,12 @@ export class CategoryComponent {
 
   @Input() category: Category;
 
-  constructor() {
+  constructor(private categoryP : CategoryProvider) {
     console.log('Hello CategoryComponent Component');
+  }
+
+  remove(id: number){
+    this.categoryP.remove(id);
   }
 
 }
